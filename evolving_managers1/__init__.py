1eaa42ec9c99c435b005188bc825298b2392c3d3
Johannes Leutgeb <johannes.leutgeb@wzb.eu>
2022-08-23 16:26:19 +0200

    15 rounds again

--- evolving_managers1/__init__.py
+++ evolving_managers1/__init__.py
@@ -12,7 +12,7 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'evolving_managers1'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 2 # number of supergames
+    NUM_ROUNDS = 15 # number of supergames
     POPULATION_SIZE = 6 # population size/matching silo size
     ACTION_DECIMAL_PLACES = 3 # how fine is the action grid (bounded by 0 and 1). with 3 it's 0, 0.001, 0.002, etc
 
