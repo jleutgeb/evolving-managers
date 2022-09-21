cf793d19d2d1a2fc9a6ec51666853e1a1e1f7a2b
Johannes Leutgeb <johannes.leutgeb@wzb.eu>
2022-09-21 15:32:15 +0200

    update to 40 supergames

--- evolving_managers1/__init__.py
+++ evolving_managers1/__init__.py
@@ -12,7 +12,7 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'evolving_managers1'
     PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 20 # number of supergames
+    NUM_ROUNDS = 40 # number of supergames
     POPULATION_SIZE = 8 # population size/matching silo size
     ACTION_DECIMAL_PLACES = 3 # how fine is the action grid (bounded by 0 and 1). with 3 it's 0, 0.001, 0.002, etc
     GAMMA = 0.0 # substitutability
