b180da71925e4ef0e76e8f4c6010b244485530fe
Johannes Leutgeb <johannes.leutgeb@wzb.eu>
2023-02-08 16:23:57 +0100

    finish

--- test-high-frequency.js
+++ test-high-frequency.js
@@ -1,10 +1,11 @@
-js_vars.confidence = 1;
-js_vars.partner_confidence = 1.1;
-js_vars.joint_payoff_info = true;
-js_vars.relative_payoff_info = true;
+js_vars.confidence = 1.1;
+js_vars.partner_confidence = 1.0;
 
-var p1_fakeaction = 0.3;
-var p2_fakeaction = 0.4;
+myChart.data.datasets[6].hidden = false
+myChart.data.datasets[8].hidden = false
+
+var p1_fakeaction = 0.35;
+var p2_fakeaction = 0.25;
 
 var p1_fakepayoff = payoff(js_vars.confidence, p1_fakeaction, p2_fakeaction, gamma = js_vars.gamma)
 var p2_fakepayoff = payoff(js_vars.partner_confidence, p2_fakeaction, p1_fakeaction, gamma = js_vars.gamma)
